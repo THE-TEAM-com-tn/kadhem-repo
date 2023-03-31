bc53500dbb70064a97472f27ed4b56be95ef04e7
SELLAMI Kadhem <97824122+spartanathlete@users.noreply.github.com>
2023-03-31 04:03:10 +0100

    Update image_card.dart
    Fixed missing file "image_card..."

--- lib/image_card.dart
+++ lib/image_card.dart
@@ -2,4 +2,3 @@ library image_card;
 
 export 'package:image_card/src/fill_image_card.dart';
 export 'package:image_card/src/transparent_image_card.dart';
-export 'package:image_card/src/image_card_widget.dart';

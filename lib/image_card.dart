7709ec79b8dcdef43abe1e680234c4497bf6578f
SELLAMI Kadhem <97824122+spartanathlete@users.noreply.github.com>
2023-03-31 01:57:19 +0100

    Pushing image_card_widget package to my repo

--- lib/image_card.dart
+++ lib/image_card.dart
@@ -2,3 +2,4 @@ library image_card;
 
 export 'package:image_card/src/fill_image_card.dart';
 export 'package:image_card/src/transparent_image_card.dart';
+export 'package:image_card/src/image_card_widget.dart';

48139b432c429fac74cb9f04f1f9114672d92af5
Med Amine bt <60607576+iifast2@users.noreply.github.com>
2023-04-15 12:15:45 +0100

    Commit amine - #2
    Commit amine - #2
    
    :white_check_mark: Done
    - settings page ( Fixed UI buttons and redirections )
    - setup index.html
    - setup db with flutterfire ( google-services.json )
    - setup Android Gradle Files
    
    :x: Not yet
    
    - Google Auth API ( activatation )
    - Firebase Rules ( accept all requests )
    - android keytool - SHA1

--- lib/UI/Pages/SettingsPage/Settings.dart
+++ lib/UI/Pages/SettingsPage/Settings.dart
@@ -2,6 +2,7 @@ import 'package:elearning_provider/UI/Pages/EditProfilePage/EditProfilePage.dart
 import 'package:elearning_provider/UI/Pages/categories/list_categories.dart';
 import 'package:elearning_provider/UI/Pages/tags/list_tags.dart';
 import 'package:elearning_provider/UI/Pages/trainings/list_trainings.dart';
+import 'package:elearning_provider/auth/home_page.dart';
 import 'package:elearning_provider/models/UserModel.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -17,70 +18,152 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.deepPurple[200], // Set the background color to deep purple
+        elevation: 0,
+
+
+        leading: IconButton(
+          icon: const Icon(Icons.account_box, color: Colors.white),
+          onPressed: () {
+
+            // This Icon button will redirect you to the user profile page or User's Settings Page !
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomePage(),
+              ),
+            );
+
+          },
+        ),
+
+
+
+
+
         title: const Center(
           child: Text( "welcome",
             //user.email!, // this will display the user email in the app bar
             style: TextStyle(fontSize: 16),
           ),
         ),
-        backgroundColor: Colors.deepPurple[200],
-        elevation: 0,
+
+
         actions: [
+
+
           TextButton.icon(
+
             onPressed: signUserOut,
+
             icon: const Icon(Icons.logout, color: Colors.white),
             label: const Text(
               "Logout",
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 20,
+                fontSize: 15,
               ),
             ),
           ),
+
         ],
+
+
       ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
 
-      body: Row(children: [
-        const SizedBox(
-          height: 60,
-        ),
-        ElevatedButton(
+          const SizedBox(height: 6),
+
+              const Text(
+                "This is the Weclome/Home Page , these buttons will be on the Side NavBar",
+                style: TextStyle(fontSize: 20),
+              ),
+
+              const SizedBox(height: 16),
+
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const EditProfilePage()));
+              },
+              child: const Text("Edit Profile Page")
+          ),
+
+          const SizedBox(height: 15),
+
+
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const ChangePasswordPage()));
+              },
+              child: const Text("Change Password Page")
+          ),
+
+
+          const SizedBox(height: 15),
+
+
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const ListTags()));
+              },
+              child: const Text("Tags page")
+          ),
+
+          const SizedBox(height: 15),
+
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const ListCategories()));
+              },
+              child: const Text("Categories page")
+          ),
+
+          const SizedBox(height: 15),
+
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const ListTrainings()));
+              },
+              child: const Text("Trainings page")
+          ),
+
+          const SizedBox(height: 15),
+
+
+          ElevatedButton(
             onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const EditProfilePage()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ),
+              );
             },
-            child: const Text("Edit Profile Page")),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const ChangePasswordPage()));
-            },
-            child: const Text("Change Password Page")),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => const ListTags()));
-            },
-            child: const Text("Tags page")),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const ListCategories()));
-            },
-            child: const Text("Categories page")),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const ListTrainings()));
-            },
-            child: const Text("Trainings page")),
-        ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut() ;
-            },
-            child: const Text("Sign Out")),
-      ]),
+            child: const Text("HomePage / User Profile")
+          ),
+
+          const SizedBox(height: 15),
+
+          ElevatedButton(
+              onPressed: () {
+                print("this button does nothing");
+              },
+              child: const Text("Button Sample")
+          ),
+
+
+
+            ]),
+      ),
     );
   }
 
